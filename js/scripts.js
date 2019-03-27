06eac53c6b49cb73edc8c070f0e2d20c7a55a7ec
mugukamil <mugukamil@gmail.com>
2019-03-28 01:16:33 +0300

    init

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInRight');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInLeft');
+	}, {
+		offset: '50%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInRight');
+	}, {
+		offset: '50%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInLeft');
+	}, {
+		offset: '50%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInRight');
+	}, {
+		offset: '50%'
+	});
+	$('.wp7').waypoint(function() {
+		$('.wp7').addClass('animated fadeInLeft');
+	}, {
+		offset: '50%'
+	});
+	$('.wp8').waypoint(function() {
+		$('.wp8').addClass('animated fadeInRight');
+	}, {
+		offset: '50%'
+	});
+	$('.wp9').waypoint(function() {
+		$('.wp9').addClass('animated fadeInLeft');
+	}, {
+		offset: '50%'
+	});
+	$('.wp10').waypoint(function() {
+		$('.wp10').addClass('animated fadeInRight');
+	}, {
+		offset: '50%'
+	});
+	$('.wp11').waypoint(function() {
+		$('.wp11').addClass('animated fadeInLeft');
+	}, {
+		offset: '50%'
+	});
+
+});
